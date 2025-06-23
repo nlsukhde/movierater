74e9e71cdf813324a322d3a4435293026a3a6ae0
nlsukhde <102875950+nlsukhde@users.noreply.github.com>
2025-06-23 17:47:15 -0400

    swapped to typescript

---
+++ client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>MovieRater</h1>
+      <h2>Whats up</h2>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+        <Button>Click me</Button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
