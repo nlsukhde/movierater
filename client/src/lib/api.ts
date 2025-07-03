4d6318cee43160387a8147d6e2152baa8f8947f8
nlsukhde <102875950+nlsukhde@users.noreply.github.com>
2025-07-02 21:19:50 -0400

    changed api link

--- client/src/lib/api.ts
+++ client/src/lib/api.ts
@@ -3,7 +3,7 @@ import { supabase } from "./supabaseClient";
 
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://movierater-el26.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
