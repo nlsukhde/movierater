f60eb49b108e20be5738d85f241883927df81b86
nlsukhde <102875950+nlsukhde@users.noreply.github.com>
2025-07-02 22:26:02 -0400

    fixed redundancy

--- client/src/components/MovieGrid.tsx
+++ client/src/components/MovieGrid.tsx
@@ -1,10 +1,6 @@
 // MovieGrid.tsx
 import React from "react";
 
-interface MovieGridProps {
-  children: React.ReactNode;
-}
-
 export function MovieGrid({ children }: { children: React.ReactNode }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
